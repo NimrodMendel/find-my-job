1a2a845c86a674adb02d334df63ce275f36e6b7f
NimrodMendel <mendel.nimrod@gmail.com>
2023-05-17 00:23:54 +0300

    Change port number for vite

--- packages/backend/main.py
+++ packages/backend/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    'http://localhost:3000'
+    'http://localhost:5173'
 ]
 
 app.add_middleware(CORSMiddleware, allow_origins=origins,
